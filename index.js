48a504365d75fb7b89115abee48ac18b10095599
Rene Cocom <fasnimo@gmail.com>
2019-02-18 18:46:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ happyHolidaysTo(name) {
 happyHolidaysTo(name) {
   console.log("Happy Holidays" + 'Mary');
 }
+
+happyHolidaysCountdown(name) {
+  console.log("It's ${5} days until ${New Year}");
+}
