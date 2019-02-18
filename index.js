cb6900c3f61049a094d02e9ee6c35e16a7f64f3a
Rene Cocom <fasnimo@gmail.com>
2019-02-18 19:14:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // write your code below!
 function happyHolidays(holidays) {
-  console.log();
+  console.log("Happy Holidays!");
 
 happyHolidaysTo(name); {
-  console.log("John");
+  console.log("Happy Holidays" + "John");
 }
 
 happyHolidaysTo(holidays,name); {
-  console.log('Independence Day', 'you');
+  console.log('Independence Day' + "John");
 }
 
 happyHolidaysCountdown(holidays,days); {
-  console.log ("Mother's Day", 20);
+  console.log ("Mother's Day" + "20");
 }
 
 }
